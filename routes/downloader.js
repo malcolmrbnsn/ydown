fc7e362242cba2e02a8383c13d2c1419ab2a0257
Malcolm Robinson <34327237+robthr@users.noreply.github.com>
2021-07-08 11:09:15 +1000

    update

--- routes/downloader.js
+++ routes/downloader.js
@@ -1,10 +1,11 @@
 const router = require("express").Router(),
-  Downloader = require("../middleware/downloader");
+  Downloader = require("../middleware/downloader"),
+  { isLoggedIn } = require("../middleware/auth");
 
 // Start the scheduler
 let downloader = new Downloader();
 
-router.get("/options", async (req, res) => {
+router.get("/options", isLoggedIn, async (req, res) => {
   let { hours, minutes, enabled } = downloader.options;
   currentTime = hours + ":" + minutes;
   return res.render("auth/options", {
@@ -14,7 +15,7 @@ router.get("/options", async (req, res) => {
   });
 });
 
-router.post("/options", (req, res) => {
+router.post("/options", isLoggedIn, (req, res) => {
   // get settings from the request
   let [hours, minutes] = req.body.downloadTime.split(":");
   let enabled = req.body.enabled === "1";
