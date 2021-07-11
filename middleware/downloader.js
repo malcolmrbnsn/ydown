3914fd523ce8121c4e00c7449ac1fe2cec6483de
robthr <calumrobinson9@gmail.com>
2021-07-11 23:36:35 +1000

    changes

--- middleware/downloader.js
+++ middleware/downloader.js
@@ -52,7 +52,7 @@ class Downloader {
     );
 
     // reset the scheduler
-    this.scheduler.reschedule(newOptions);
+    this.scheduler.reschedule(newOptions, this.downloadVideos.vind(this));
 
     // save to file
     fs.writeFileSync("/persist/options.json", JSON.stringify(newOptions));
